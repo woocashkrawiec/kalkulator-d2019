cce28bf586802e61df77f656517adab33b890d59
Łukasz Krawiec <53347@student.ue.poznan.pl>
2019-04-08 17:40:37 +0200

    funkcja get_help gotowa do testów

--- kalkulator.py
+++ kalkulator.py
@@ -1,9 +1,16 @@
 print('to jest prosty kalkulator')
 
+def get_help():
+	print('Podaj dwie liczby:')
+
+
+
 def dodaj():
 	a = int(input())
 	b = int(input())
 	print(a+b)
 	
-
+get_help()
 print ('Koniec programu')
+
+
